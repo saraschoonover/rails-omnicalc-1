d73b29ef0cd9b0a632cbd66618e268adef9a95a0
Sara Schoonover <Saramschoon@gmail.com>
2023-09-26 21:00:36 +0000

    corrected ids

--- app/controllers/submittedsquare_controller.rb
+++ app/controllers/submittedsquare_controller.rb
@@ -1,6 +1,6 @@
 class SubmittedsquareController < ApplicationController 
   def submittedSquare
-    @num = params.fetch("square").to_f
+    @num = params.fetch("number").to_f
     @squared = @num * @num
     render({:template => "submitted_templates/subsquare"})
   end
